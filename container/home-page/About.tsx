f9a38556ffb703b99eb045c067df3eed5e6e4d7b
alpha-aks <149251160+alpha-aks@users.noreply.github.com>
2025-11-12 10:12:27 +0530

    Style: set 'Our approach' heading to navy blue to match section

--- container/home-page/About.tsx
+++ container/home-page/About.tsx
@@ -71,7 +71,7 @@ export default function About() {
 			</div>
 			<div className="w-full flex justify-between padding-x sm:flex-col xm:flex-col gap-[30px]">
 				<div className="flex flex-col gap-[30px]">
-					<Heading title="Our approach:" />
+					<Heading title="Our approach:" className="text-[#0a2540]" />
 					<div
 						className="w-fit flex items-center justify-between bg-secondry cursor-pointer rounded-full group"
 						onMouseEnter={() => setHovered(true)}
